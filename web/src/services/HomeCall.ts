5b7c7c83a03ce8dfdf64bf9e9af9f86803cd7ed8
Alban de Tourtier <alban.detourtier@bring4you.com>
2023-09-18 18:53:59 +0100

    [WEB][ADD] Work Experience component

--- web/src/services/HomeCall.ts
+++ web/src/services/HomeCall.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const getHomeDataType = async (type: string) => (
     axios.get(`http://localhost:3001/api/home/${type}`)
     .then(function (response) {
-        console.log(`http://localhost:3001/api/home/${type}`)
         return response.data;
     })
     .catch(function (error) {
