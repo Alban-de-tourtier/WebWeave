8a463767dfb6ab14295bc95d4f1486715a7b73b7
Alban de Tourtier <alban.detourtier@bring4you.com>
2023-09-18 17:57:48 +0100

    [WEB][ADD] Services Providing component

--- web/src/app/(pages)/homeWIP/page.tsx
+++ web/src/app/(pages)/homeWIP/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import AboutMe from '@/app/_component/homeComponent/AboutMe/AboutMe';
 import Header from '@/app/_component/homeComponent/Header/Header';
+import ServiceProviding from '@/app/_component/homeComponent/ServiceProviding/ServiceProviding';
 
 const AdminPanel = () => {
     return (
@@ -8,6 +9,7 @@ const AdminPanel = () => {
             <div>
                 <Header />
                 <AboutMe />
+                <ServiceProviding />
             </div>
         </main>
     )
