7812984b253b0d241ea6d0df6cd74624eae3694b
Alban de Tourtier <alban.detourtier@bring4you.com>
2023-09-21 21:24:19 +0100

    [WEB][MODIFY] AWS ERROR

--- api/src/db/mongoose.ts
+++ api/src/db/mongoose.ts
@@ -1,8 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose, { ConnectOptions } from 'mongoose';
 
-mongoose.connect('mongodb+srv://Alban:mYzuuRN2sKMKwlIx@webweave.pf5m2cw.mongodb.net/WebWeave', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://Alban:mYzuuRN2sKMKwlIx@webweave.pf5m2cw.mongodb.net/WebWeave');
 
 export default mongoose;
