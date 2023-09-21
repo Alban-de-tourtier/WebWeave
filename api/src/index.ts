be79c6e31ee080c8fd9cbed9bd2565ee5dcdc45e
Alban de Tourtier <alban.detourtier@bring4you.com>
2023-09-21 14:07:44 +0100

    [API][ADD] contact router

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,9 +3,10 @@ import bodyParser from 'body-parser';
 import aboutRouter from './routes/about';
 import homeRouter from './routes/home';
 import experiencesRouter from './routes/experiences';
+import contactRouter from './routes/contact';
 
 const app = express();
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 const corsOptions = {
   origin: `${process.env.WEBURL}:3000`,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -15,12 +16,13 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(cors(corsOptions)); // Use the cors middleware with your options
+app.use(cors(corsOptions));
 
 // API routes
 app.use('/api/home', homeRouter);
 app.use('/api/about', aboutRouter);
 app.use('/api/experiences', experiencesRouter);
+app.use('/api/contact', contactRouter);
 
 const PORT = process.env.PORT || 3001;
 
